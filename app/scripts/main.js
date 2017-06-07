b1f9a2c434caeace1137bf22ee51f7401453a951
patrickgrey <websemantic@yahoo.co.uk>
2017-06-07 21:50:36 +0100

    Removed call to Modernizr.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,16 @@ var AndyMulholland = (function ($) {
 
     var module              = {};
 
-    var IEReplaceSVGPNG = function () {
-        $('img[src*="svg"]').attr('src', function() {
-            return $(this).attr('src').replace('.svg', '.png');
-        });
-    };
+    // var IEReplaceSVGPNG = function () {
+    //     $('img[src*="svg"]').attr('src', function() {
+    //         return $(this).attr('src').replace('.svg', '.png');
+    //     });
+    // };
 
     module.init = function () {
-        if(!Modernizr.svg) {
-            IEReplaceSVGPNG();
-        }
+        // if(!Modernizr.svg) {
+        //     IEReplaceSVGPNG();
+        // }
         $('body').scrollspy({ target: '.navbar-default' });
     };
 
